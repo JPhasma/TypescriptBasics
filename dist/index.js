fafbd9b88f492035a402e749084e96e27faf5e49
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-07-17 18:00:14 +0100

    Objects with types defined

--- dist/index.js
+++ dist/index.js
@@ -24,3 +24,45 @@ employee = [
     [3, 'Ja'],
 ];
 console.log('Tuple array example:', employee);
+// Union - allows for more than one Type should you really need it
+var flipFlop = 'this string could also be a number if needed';
+console.log('Union Type demoed by flipFlop', flipFlop);
+flipFlop = 50;
+console.log('Union Type demoed by flipFlop', flipFlop);
+flipFlop = 'Was a string, then a number, back to a string again';
+console.log('Union Type demoed by flipFlop', flipFlop);
+// Enum - allow to define a set of named constants, either numberic or string
+// Direction1 has numeric assigned enumerations
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log('Enum example - Direction1.Up:', Direction1.Up);
+console.log('Enum example - Direction1.Down:', Direction1.Down);
+console.log('Enum example - Direction1.Left:', Direction1.Left);
+console.log('Enum example - Direction1.Right:', Direction1.Right);
+// Direction2 has string assigned enumerations
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log('Enum example - Direction2.Up:', Direction2.Up);
+console.log('Enum example - Direction2.Down:', Direction2.Down);
+console.log('Enum example - Direction2.Left:', Direction2.Left);
+console.log('Enum example - Direction2.Right:', Direction2.Right);
+// Objects can be defined by types
+var user = {
+    id: 1,
+    name: 'John',
+};
+var car = {
+    id: 1,
+    make: 'Nissan',
+};
+console.log(car.make);
--- src/index.ts
+++ src/index.ts
@@ -36,5 +36,53 @@ console.log('Tuple array example:', employee);
 
 // Union - allows for more than one Type should you really need it
 let flipFlop: string | number = 'this string could also be a number if needed';
+console.log('Union Type demoed by flipFlop', flipFlop);
 flipFlop = 50;
+console.log('Union Type demoed by flipFlop', flipFlop);
 flipFlop = 'Was a string, then a number, back to a string again';
+console.log('Union Type demoed by flipFlop', flipFlop);
+
+// Enum - allow to define a set of named constants, either numberic or string
+// Direction1 has numeric assigned enumerations
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+console.log('Enum example - Direction1.Up:', Direction1.Up);
+console.log('Enum example - Direction1.Down:', Direction1.Down);
+console.log('Enum example - Direction1.Left:', Direction1.Left);
+console.log('Enum example - Direction1.Right:', Direction1.Right);
+
+// Direction2 has string assigned enumerations
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+console.log('Enum example - Direction2.Up:', Direction2.Up);
+console.log('Enum example - Direction2.Down:', Direction2.Down);
+console.log('Enum example - Direction2.Left:', Direction2.Left);
+console.log('Enum example - Direction2.Right:', Direction2.Right);
+
+// Objects can be defined by types
+const user: { id: number; name: string } = {
+  id: 1,
+  name: 'John',
+};
+
+// can define an object also by creating a type first
+type Car = {
+  id: number;
+  make: string;
+};
+
+const car: Car = {
+  id: 1,
+  make: 'Nissan',
+};
+console.log(car.make);
