c243ede0b332cf9ee4fa514553585e6fdd9c0875
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-07-17 16:46:36 +0100

    array Types

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,7 @@ var id = 6;
 var company = 'Weyland Yutani';
 var isDangerous = true;
 console.log('ID:', id, 'Company:', company, 'IsDangerous?: ', isDangerous);
+// Type for an array of numbers
+var ids = [1, 2, 3, 4, 5];
+// use 'any' to be able to have a mixed array
+var arr = [1, 2, '3', 'mixed things'];
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ let company: string = 'Weyland Yutani';
 let isDangerous: boolean = true;
 
 console.log('ID:', id, 'Company:', company, 'IsDangerous?: ', isDangerous);
+
+// Type for an array of numbers
+let ids: number[] = [1, 2, 3, 4, 5];
+
+// use 'any' to be able to have a mixed array
+let arr: any[] = [1, 2, '3', 'mixed things'];
