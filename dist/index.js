8d12c4cb4d2da79c2ebf6228d4215439b78bf971
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-07-17 16:41:38 +0100

    main primitive Types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
+//basic types
 var id = 6;
-console.log('ID:', id);
+var company = 'Weyland Yutani';
+var isDangerous = true;
+console.log('ID:', id, 'Company:', company, 'IsDangerous?: ', isDangerous);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+//basic types
 let id: number = 6;
+let company: string = 'Weyland Yutani';
+let isDangerous: boolean = true;
 
-console.log('ID:', id);
+console.log('ID:', id, 'Company:', company, 'IsDangerous?: ', isDangerous);
