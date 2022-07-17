b8902d38951264382c65ea52643f7f7886d7d6b1
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-07-17 16:54:47 +0100

    Tuples and Tuple Arrays

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,20 @@ console.log('ID:', id, 'Company:', company, 'IsDangerous?: ', isDangerous);
 // Type for an array of numbers
 var ids = [1, 2, 3, 4, 5];
 // use 'any' to be able to have a mixed array
-var arr = [1, 2, '3', 'mixed things'];
+var arr = [1, 2, '3', 'mixed things', true];
+// Tuple - can define the Types in an array
+var person = [
+    5,
+    'Crew Members',
+    true,
+    { expendable: true },
+];
+console.log('Tuple example - eg: must be a number, string, boolean, object', person);
+// Tuples can then be defined into an array (an array of Tuples)
+var employee;
+employee = [
+    [1, 'Jane'],
+    [2, 'Rane'],
+    [3, 'Ja'],
+];
+console.log('Tuple array example:', employee);
--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,27 @@ console.log('ID:', id, 'Company:', company, 'IsDangerous?: ', isDangerous);
 let ids: number[] = [1, 2, 3, 4, 5];
 
 // use 'any' to be able to have a mixed array
-let arr: any[] = [1, 2, '3', 'mixed things'];
+let arr: any[] = [1, 2, '3', 'mixed things', true];
+
+// Tuple - can define the Types in an array
+let person: [number, string, boolean, object] = [
+  5,
+  'Crew Members',
+  true,
+  { expendable: true },
+];
+
+console.log(
+  'Tuple example - eg: must be a number, string, boolean, object',
+  person
+);
+
+// Tuples can then be defined into an array (an array of Tuples)
+let employee: [number, string][];
+employee = [
+  [1, 'Jane'],
+  [2, 'Rane'],
+  [3, 'Ja'],
+];
+
+console.log('Tuple array example:', employee);
